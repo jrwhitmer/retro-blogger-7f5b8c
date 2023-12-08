2e3cdda65495400bbd34283a7ee2bb9bc51098c9
Jason Barry <jb@netlify.com>
2023-12-07 19:12:48 -0800

    Only a single index.html file

--- netlify/functions/hello-world.ts
+++
@@ -1,9 +0,0 @@
-import { Config, Context } from "@netlify/functions"
-
-export default async (req: Request, context: Context) => {
-  return new Response(`Hello, ${context.ip}!`)
-}
-
-export const config: Config = {
-  path: "/api/hello"
-}
